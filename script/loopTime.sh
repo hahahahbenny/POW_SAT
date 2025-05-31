adc76c02860d75c59155530bbe7b4a6a1514a833
bennyWu <benny066567@163.com>
2025-05-31 22:00:12 +0800

    change script with average

--- script/loopTime.sh
+++ script/loopTime.sh
@@ -4,7 +4,7 @@
 LOOP_COUNT=8
 
 
-SAT_NAME=("KISSAT" "CRYPTOMINISAT" "CADICAL" "KISSAT24" "BREAKID" "HKIS" "HKIS_PBVA" "CADICAL_BVA" "CADICAL_SCAVEL" "CADICAL_VIVINST")
+SAT_NAME=("KISSAT" "CRYPTOMINISAT" "CADICAL" "KISSAT24" "BREAKID" "HKIS" "HKIS_PBVA" "CADICAL_BVA" "CADICAL_SCAVEL" "CADICAL_VIVINST" "MapleCaDiCaL_PPD")
 SAT_ADRESS=("/home/bennywu/benny_workflow/cpp_learning/sat/kissat-master/build/kissat" \
             "cryptominisat5" \
             "/home/bennywu/benny_workflow/cpp_learning/sat/cadical-master/build/cadical" \
@@ -15,51 +15,68 @@ SAT_ADRESS=("/home/bennywu/benny_workflow/cpp_learning/sat/kissat-master/build/k
             "/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/SBVA/bin/starexec_run_sbva_cadical" \
             "/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/Cadical_rel_1.5.3.Scavel/bin/starexec_run_default" \
             "/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/CaDiCaL_vivinst/bin/starexec_run_default"
+            "/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/MapleCaDiCaL_PPD/bin/starexec_run_500_500"
             )
 
-# 设置求解器
-KISSAT="/home/bennywu/benny_workflow/cpp_learning/sat/kissat-master/build/kissat"
-CRYPTOMINISAT="cryptominisat5"
-CADICAL="/home/bennywu/benny_workflow/cpp_learning/sat/cadical-master/build/cadical"
-BREAKID="/home/bennywu/benny_workflow/cpp_learning/sat/sequential/BreakID-Kissat/bin/starexec_run_default"
-KISSAT24="/home/bennywu/benny_workflow/cpp_learning/sat/sequential/kissat-sc2024/bin/kissat"
-HKIS="/home/bennywu/benny_workflow/cpp_learning/sat/sequential/hKis/bin/starexec_run_bva"
-HKIS_PBVA="/home/bennywu/benny_workflow/cpp_learning/sat/sequential/hKis/bin/starexec_run_pbva"
-CADICAL_BVA="/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/SBVA/bin/starexec_run_sbva_cadical"
-CADICAL_SCAVEL="/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/Cadical_rel_1.5.3.Scavel/bin/starexec_run_default"
-CADICAL_VIVINST="/home/bennywu/benny_workflow/cpp_learning/sat/Sequential2023/Sequential/solvers/CaDiCaL_vivinst/bin/starexec_run_default"
+CNF_FOLDER="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/output_cnf/withour_xor"
 
-SAT=$CADICAL_VIVINST
+CNF_CLASS=("sat" \
+            "optimize_with_k2" \
+            "OptimizeVars_a_to_h" \
+            "constant_bool"
+)
+
+SUM=0
 
 # 清空输出文件（如果已存在）
 TEST_FILE="/home/bennywu/benny_workflow/cpp_learning/satcoin-master/out_1k_sat.cnf"
 
 # 循环执行指令
-for ((j = 0; j < 10; j++)); do
-    OUTPUT_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/result/sat/optimize/consume_time_optimize_${SAT_NAME[j]}.csv"
-    > "$OUTPUT_FILE"
-    for ((i = 14; i <= 16; i++)); do
-        echo "Iteration $j: $i"
+for ((j = 0; j < 11; j++)); do
+    for((h = 0; h < 4; h++)); do
+        OUTPUT_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/result/sat/${CNF_CLASS[h]}/consume_time_${SAT_NAME[j]}.csv"
+        > OUTPUT_FILE
+        echo "bits, time(avg)" >> "$OUTPUT_FILE"
+        for ((i = 4; i <= 10; i++)); do
+            echo "Iteration $j: $i"
+            echo  "${SAT_NAME[j]} : ${SAT_ADRESS[j]}"
+            
+            TEMP_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/result/sat/${CNF_CLASS[h]}/temp/consume_time_${SAT_NAME[j]}_$((i))_bit.txt"
+            > "$TEMP_FILE"
 
-        echo  "${SAT_NAME[j]} : ${SAT_ADRESS[j]}"
+            CNF_FILE="$CNF_FOLDER/${CNF_CLASS[h]}/$((i))bits_left.cnf"
+            SUM=0
+            for((k = 0; k < 1000; k++)); do
+                if [ $j -gt 3 ]; then
+                    > "/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/code/script/temp/proof.out"
+                    timeout -s SIGINT 1800s /usr/bin/time -f "$((i))_bits, %e \n" -a -o "$TEMP_FILE" \
+                    ${SAT_ADRESS[j]} "$CNF_FILE" /home/bennywu/benny_workflow/cpp_learning/satcmpbrute/code/script/temp
+                else
+                    timeout -s SIGINT 1800s /usr/bin/time -f "$((i))_bits, %e \n" -a -o "$TEMP_FILE" \
+                    ${SAT_ADRESS[j]} "$CNF_FILE" 
+                fi
 
-        > "/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/code/script/temp/proof.out"
+                TIME=$(tail -n 2 "$TEMP_FILE" | awk -F ',' '{print $2}')
+                
+                if [[ -z "$TIME" ]]; then
+                    echo "Warning: TIME variable is empty. Skipping this iteration."
+                    continue
+                fi
 
-        CNF_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/output_cnf/OptimizeVars_a_to_h/$((i))bits_left.cnf"
-        # RESULT_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/result/${SAT_NAME[j]}_optimize.txt"
-        
-        # echo $CNF_FILE /home/bennywu/benny_workflow/cpp_learning/satcmpbrute/script/temp
+                # 累加时间
+                SUM=$(echo "$SUM + $TIME" | bc)
+            done
+            # RESULT_FILE="/home/bennywu/benny_workflow/cpp_learning/satcmpbrute/result/${SAT_NAME[j]}_optimize.txt"
 
-        if [ $j -gt 3 ]; then
-            timeout -s SIGINT 1800s /usr/bin/time -f "$((i))_bits, %e \n" -a -o "$OUTPUT_FILE" \
-            ${SAT_ADRESS[j]} "$CNF_FILE" /home/bennywu/benny_workflow/cpp_learning/satcmpbrute/code/script/temp
-        else
-            timeout -s SIGINT 1800s /usr/bin/time -f "$((i))_bits, %e \n" -a -o "$OUTPUT_FILE" \
-            ${SAT_ADRESS[j]} "$CNF_FILE" 
-        fi
-        # # 使用 time 命令记录执行时间，将结果写入文件
+            # echo $CNF_FILE /home/bennywu/benny_workflow/cpp_learning/satcmpbrute/script/temp
+            # # 使用 time 命令记录执行时间，将结果写入文件
 
-        echo "Iteration $((i)) completed."
+            AVERAGE=$(echo "$SUM / 1000" | bc -l)
+            echo "$((i)), $AVERAGE" >> "$OUTPUT_FILE"
+            echo "$((i)) average time is $AVERAGE"
+            echo "Iteration $((i)) completed."
+            echo $SUM
+        done
     done
 done
 
